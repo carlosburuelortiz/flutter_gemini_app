f08324069c13d9b5bd691ec4225b1d5f52fef147
Carlos Buruel <charly.ob.26@gmail.com>
2025-11-13 19:37:00 -0600

    9. Tema global

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gemini_app/config/router/app_router.dart';
+import 'package:gemini_app/config/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,6 +14,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
+      theme: AppTheme(isDarkMode: true).getTheme(),
     );
   }
 }
