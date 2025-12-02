b25c369b4d728b4cc1d1f3ba16151f50feebd09f
Carlos Buruel <charly.ob.26@gmail.com>
2025-12-02 14:00:23 -0600

    28. Variables de entorno - BaseURL

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:gemini_app/config/router/app_router.dart';
 import 'package:gemini_app/config/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+
   runApp(ProviderScope(child: MainApp()));
 }
 
