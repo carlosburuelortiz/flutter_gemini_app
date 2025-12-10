5cf78d11dd7a5b3a7b989efbc22e0ec34609d653
Carlos Buruel <charly.ob.26@gmail.com>
2025-12-09 19:40:55 -0600

    53. Selección de imágenes

--- lib/presentation/screen/basic_prompt/basic_prompt_screen.dart
+++ lib/presentation/screen/basic_prompt/basic_prompt_screen.dart
@@ -6,6 +6,8 @@ import 'package:gemini_app/presentation/provider/chat/basic_chat.dart';
 import 'package:gemini_app/presentation/provider/chat/is_gemini_writing.dart';
 
 import 'package:gemini_app/presentation/provider/user/user_provider.dart';
+import 'package:gemini_app/presentation/widget/chat/custom_bottom_input.dart';
+import 'package:image_picker/image_picker.dart';
 
 class BasicPromptScreen extends ConsumerWidget {
   const BasicPromptScreen({super.key});
@@ -24,12 +26,26 @@ class BasicPromptScreen extends ConsumerWidget {
 
         // On send message
         onSendPressed: (types.PartialText partialText) {
-          final basicChatNotifier = ref.read(basicChatProvider.notifier);
-          basicChatNotifier.addMessage(partialText: partialText, user: user);
+          // final basicChatNotifier = ref.read(basicChatProvider.notifier);
+          // basicChatNotifier.addMessage(partialText: partialText, user: user);
         },
         user: user,
         theme: DarkChatTheme(),
         showUserNames: true,
+        customBottomWidget: CustomBottomInput(
+          onSend: (partialText, {images = const []}) {
+            final basicChatNotifier = ref.read(basicChatProvider.notifier);
+            basicChatNotifier.addMessage(partialText: partialText, user: user);
+            print(images);
+          },
+        ),
+        // onAttachmentPressed: () async {
+        //   ImagePicker picker = ImagePicker();
+        //   final List<XFile> images = await picker.pickMultiImage(limit: 4);
+        //   if (images.isEmpty) return;
+
+        //   print(images);
+        // },
         // showUserAvatars: true,
         typingIndicatorOptions: TypingIndicatorOptions(
           typingUsers: isGeminiWriting ? [geminiUser] : [],
