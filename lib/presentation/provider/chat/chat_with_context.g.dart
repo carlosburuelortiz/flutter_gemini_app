5adf36c628ce35ecee1d5890599a35e02f64086a
Carlos Buruel <charly.ob.26@gmail.com>
2025-12-20 07:26:36 -0600

    70. Proveedor - Chat con contexto

---
+++ lib/presentation/provider/chat/chat_with_context.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chat_with_context.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$chatWithContextHash() => r'ca69f12553e18122e4b80eebd9d754d0ac24afd5';
+
+/// See also [ChatWithContext].
+@ProviderFor(ChatWithContext)
+final chatWithContextProvider =
+    NotifierProvider<ChatWithContext, List<Message>>.internal(
+      ChatWithContext.new,
+      name: r'chatWithContextProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$chatWithContextHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$ChatWithContext = Notifier<List<Message>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
